b0dd06b645fbd36bb201bb6a795e07f5bb912293
RUBEN <RUBEN@DESKTOP-48FBVKK>
2021-05-16 21:45:55 +0200

    prueba4

--- Actividad4/src/prueba.java
+++ Actividad4/src/prueba.java
@@ -4,6 +4,8 @@ public class prueba {
 	public static void main(String[] args) {
 		
 		System.out.println("Hola a todos.");
+		System.out.println("soy el nuevo alumno.");
+		System.out.println("me llamo pepe.");
 		}
 }
 
